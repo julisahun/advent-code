6cbc98b1c4fc9bd2852f83f632f70a081ab52417
julisahun <julisahun@gmail.com>
2025-12-07 13:13:31 +0100

    part 2

--- problem-1/extension.cpp
+++ problem-1/extension.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 #include <string>
 
+const int INITIAL_POSITION = 50;
+const int mod = 100;
+
 int main() {
     int r = 0;
-    const int INITIAL_POSITION = 50;
     int current_position = INITIAL_POSITION;
     std::string input;
+    bool was_zero = false;
     while (std::cin >> input) {
         char direction = input[0];
         int number = std::stoi(input.substr(1));
+        
         if (direction == 'R') {
             current_position += number;
         } else {
             current_position -= number;
         }
-        if (current_position % 100 == 0) r++;
+
+        r += abs(current_position / mod);
+        r += current_position <= 0 && !was_zero;
+
+        current_position = (current_position % mod + mod) % mod;
+        was_zero = current_position == 0;
     }
     std::cout << r << std::endl;
 }
